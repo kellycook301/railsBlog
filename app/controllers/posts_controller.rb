d6c85a4569b4eb10ce54721c28fcc706f988b13d
Kelly Cook <kellycook20878@gmail.com>
2019-04-12 16:08:21 -0500

    at CRUD functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-
+        @posts = Post.all
     end
 
     def show
@@ -8,14 +8,39 @@ class PostsController < ApplicationController
     end
 
     def new
+        @post = Post.new
+    end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
     end
 
     def create
         # render plain: params[:post].inspect
         @post = Post.new(post_params)
-        @post.save
-        redirect_to @post
+        
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
     end
 
     private def post_params
